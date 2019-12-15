628d9d6564d536acaf14164d18538fa029a0c34f
Peter Wildani <peter.wildani@sketchyceleryfacts.com>
2019-12-15 11:51:57 -0800

    slightly better installer

--- install.sh
+++ install.sh
@@ -1,1 +1,34 @@
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+#!/usr/bin/env zsh
+
+# Can't assume readlink -f, realpath, etc exist yet.
+# DOTFILES="$(dirname $0)"
+DOTFILES="$(echo $(cd $(dirname "$0") && pwd -P))"
+ZDOTDIR="${ZDOTDIR:-$HOME}"
+NVIM="$HOME/.config/nvim"
+
+function ensure_dir() {
+    for dirname in "$@" ; do
+        [[ -d "$dirname" ]] || mkdir -p "$dirname"
+    done
+}
+
+
+# Can't assume install exists yet.
+function install_dotfile() { 
+    ensure_dir "$(dirname "$2")"
+    if [[ -f "$2" ]] ; then
+        echo TODO: ln -s "$DOTFILES/$1" "$2"
+    else
+        ln -s "$DOTFILES/$1" "$2"
+    fi
+}
+
+
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR}/.zprezto"
+install_dotfile init.vim "$NVIM/init.vim"
+install_dotfile zshrc "$ZDOTDIR/.zshrc"
+install_dotfile zprofilerc "$ZDOTDIR/.zprofilerc"
+install_dotfile zpretzorc "$ZDOTDIR/.zpretzorc"
+
+
